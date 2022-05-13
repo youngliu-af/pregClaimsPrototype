c8421ca5e8fcbb9d9d435d3f2b12a8dfbd65f491
Young Liu <youngliu-af@gmail.com>
2022-05-13 16:35:26 -0500

    Initial Commit

---
+++ PregClaim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PregClaim
+//
+//  Created by Young Liu on 5/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PregClaim/PregClaimApp.swift
@@ -0,0 +1,17 @@
+//
+//  PregClaimApp.swift
+//  PregClaim
+//
+//  Created by Young Liu on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct PregClaimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
